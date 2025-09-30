4e30a7eba37e99261c9adaf7761e218b251340e9
John-Popovici <57932172+John-Popovici@users.noreply.github.com>
2025-10-01 01:49:03 +0200

    CI/CD testing
    * CI/CD testing
    
    * CI/CD testing
    
    * CI/CD testing
    
    * CI/CD testing

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,13 +2,16 @@ from main import square, square_wrong
 
 
 def test_square():
-    Integer = 5
-    assert square(Integer) == 25
+    integer = 5
+    assert square(integer) == 25
     assert square(1) == 1
     assert square(-5) == 25
     assert square(0) != "hello"
     
 def test_square_wrong():
-    assert square_wrong(5) == 25
-    assert square_wrong(1) == 1
-    assert square_wrong(-5) == 25
+    assert square_wrong(5) != 25
+    assert square_wrong(1) != 1
+    assert square_wrong(-5) != 25
+    
+def test_square_wrong_2():
+    assert square_wrong(2) == 4
